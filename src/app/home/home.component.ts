66b163380b0230c9de5c208d9ce2bc5c865b16ec
Ahsan Alamgir <ahsanalamgir14@gmail.com>
2024-02-23 15:58:57 +0500

    Add event on swiper buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { Router } from '@angular/router';
-import Swiper from 'swiper';
 
 register();
 
@@ -14,44 +13,26 @@ export class HomeComponent  implements OnInit {
 
   @ViewChild('swiper')
   swiperRef: ElementRef | undefined;
-  showPage: boolean = true;
-  // @ViewChild('swiper', { static: false }) swiper: any;
 
   constructor(private router: Router) { }
 
-  ngOnInit() {
-    console.log('home loaded');
-    this.showPage = true;
-    // console.log('indexx', this.swiperRef?.nativeElement.swiper.getActiveIndex());
-    // const swiper = new Swiper('.swiper-container', {
-    //   navigation: {
-    //     nextEl: '.next-button',
-    //     prevEl: '.skip-button',
-    //   },
-    // });
-  }
+  ngOnInit() { }
 
-  
   swiperChange(event: any) {
     console.log('event: ', event);
   }
 
   next() {
-    console.log(this.swiperRef?.nativeElement.swiper.activeIndex);
-    console.log(this.swiperRef?.nativeElement.swiper.previousIndex);
-    // this.swiper.slideNext();
+    const swiper = this.swiperRef?.nativeElement.swiper;
+    swiper.slideNext();
   }
 
-  
   prev() {
-    // this.swiper.slidePrev();
-    console.log(this.swiperRef?.nativeElement.swiper.nextIndex);
-
+    const swiper = this.swiperRef?.nativeElement.swiper;
+    swiper.slidePrev();
   }
 
   gotToOverview() {
-    this.showPage = false;
-    // this.router.navigate(['overview'])
     this.router.navigateByUrl('/overview');
 
   }
